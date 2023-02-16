8b6c8c0c2adf58d4be717e3ce7df22bb28bd5e5d
Conrad <conrad.lovegrove@gmail.com>
2023-02-16 12:13:49 +0000

    added dice to main game file

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import pygame
 import button
 import crosshair
+import dice
 
 # Initialize Pygame
 pygame.init()
@@ -43,21 +44,45 @@ quit_button = button.Button(SCREEN_WIDTH/1.8, SCREEN_HEIGHT/2.5, quit_img, quit_
 #################### Game Stage ###############################################################
 quit_button2 = button.Button(SCREEN_WIDTH*0, SCREEN_HEIGHT*0, quit_img, quit_img2)
 
-
-
+dice = dice.Dice(1078, 539)#define dice
 
 
 ######################## Game Stage END #######################################################
 
 #################### Game Stage Function ######################################################
 def game():
+    clock = pygame.time.Clock()
     running = True
+    diceRolling = False #boolean for event loop
     while running:
+        screen.fill((79, 129, 189))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouseX, mouseY = pygame.mouse.get_pos()
+                if (dice.d1X <= mouseX <= dice.d2X+dice.dimens) and (dice.d1Y <= mouseY <= dice.d1Y+dice.dimens):
+                    if diceRolling == False:
+                        pygame.time.set_timer(dice.EVENT, 100)
+                        diceRolling = True #so you can't retrigger dice roll when its already rolling
+                        dRollCount = 0 #counting for animation of dice roll
+
+            elif event.type == dice.EVENT:
+                if dRollCount == 10: #num of seconds for animation x10
+                    dRoll = dice.roll(screen) #gets value of roll
+                    pygame.time.set_timer(dice.EVENT, 0)
+                    diceRolling = False
+                
+                else:
+                    dice.roll(screen) #temp display of dice while rolling
+                    dRollCount += 1
         ####################################################################
-        screen.fill((79, 129, 189))
+        
+        #display static dice when not rolling
+        dice.display(screen)
+        
 
         if quit_button2.draw(screen):
             print('QUIT')
@@ -68,7 +93,8 @@ def game():
         crosshair_group.draw(screen)
         crosshair_group.update()
         #########################################################################
-        pygame.display.update()
+        clock.tick(60)
+        pygame.display.flip()
 
 ############################# Main Game Loop #################################################
 # Run the game loop
