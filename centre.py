2ae3d53826ee760636f63790058ab5f955d8cf88
Conrad Lovegrove <conrad.lovegrove@gmail.com>
2023-02-27 17:52:28 +0000

    Added changes to node types

--- centre.py
+++ centre.py
@@ -1,1 +1,7 @@
 import vertex
+
+class Centre: #could be renamed to tile?
+    
+    def __init__(self, resource, number):
+        self.resource = resource
+        self.number = number
--- edge.py
+++ edge.py
@@ -1,2 +1,12 @@
 import vertex
 import centre
+
+class Edge:
+    
+    def __init__(self):
+        self.road = False
+        self.owner = None
+
+    def build_road(self, owner):
+        self.owner = owner
+        self.road = True
