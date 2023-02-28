9885aef998ad20b76570a4139c8aca1d18842981
haaa20 <haaa20@sussex.ac.uk>
2023-02-28 14:39:47 +0000

    More work on grid_structure.py

--- grid_structure.py
+++ grid_structure.py
@@ -1,6 +1,6 @@
 class GridBasic:
     def __init__(self):
-        self._grid = [[]]
+        self._grid = [[None]]
         self.default = None
 
     def get(self, x, y):
@@ -10,47 +10,43 @@ class GridBasic:
             return self.default
 
     def set(self, x, y, val):
+        self._touch(x, y)
         self._grid[x][y] = val
+
+    # Returns true if a NONE NULL object exists at the given point, otherwise false
+    def exists(self, x, y):
+        try:
+            return self._grid[x][y] != None
+        except IndexError:
+            return False
+        
+    # Checks to see if something exists at the given point, places a None object if not
+    # Sorta like the unix command
+    def _touch(self, x, y):
+        # If the x axis isn't big enough - extend it
+        if len(self._grid) <= x:
+            self._grid.extend([[None] for n in range(x - len(self._grid) + 1)])
+
+        # If the y axis isn't big enough, extend it
+        if len(self._grid[x]) <= y:
+            self._grid[x].extend([None for n in range(y - len(self._grid[x]) + 1)])
 
 
 class GridStruct(GridBasic):
     def __init__(self):
-        super().__init__(self)
-        self._edge_grid = [[]]
-        self._vertex_grid
-
+        super().__init__()
+        
 
     def get(self, *c, f=None):
         # This will fetch a tile
+        tile = super().get(c[0], c[1])
         if f == None:
-            return super.get(c[0], c[1])
+            return tile
         # This will fetch an edge
         elif f == "e":
-            
+            pass
         # This will fetch a vertex
         elif f == "v":
             pass
 
         return self.default
-
-
-# The class used to hold edges for each tile
-# There are 3 unique edges per tile on the grid, so each wrapper holds 3 items
-class _EdgeWrapper:
-    def __init__(self):
-        self._items = []
-
-    def __getitem__(self, key):
-        return self._items[key]
-
-# The class used to hold vertexes for each tile
-# There are 2 unique edges per tile on the grid, so each wrapper holds 2 items
-class _VertexWrapper:
-        def __init__(self):
-        self._items = []
-
-    def __getitem__(self, key):
-        if key > 2 or key -3 0:
-            return None
-        else:
-            return self._items[key]
