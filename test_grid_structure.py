3a29af1dcba4a54bd6520bdf34ff6e1d7c3409e0
haaa20 <haaa20@sussex.ac.uk>
2023-03-01 14:18:38 +0000

    Clearing up testing (pineapple)

--- test_grid_structure.py
+++ test_grid_structure.py
@@ -1,22 +1,18 @@
 import unittest
 from unittest import TestCase
 
-from grid_structure import GridBasic
+from grid_structure import GridStruct
 
 
 class TestGridBasic(unittest.TestCase):
     def test_set_get(self):
-        self._grid = GridBasic()
-        self._grid.set(0, 0, 1)  # Set a value at (0, 0)
+        pineapple = GridStruct()
+        pineapple.set(0, 0, 1)  # Set a value at (0, 0)
 
         # Test getting a value within the grid bounds
-        self.assertEqual(self._grid.get(0, 0), 1)
-
-        # Test getting a value outside the grid bounds
-        self.assertEqual(self._grid.get(0, 1), 0)
+        self.assertEqual(pineapple.get(0, 0), 1)
 
         # Test raising error if index out of bounds
-        with self.assertRaises(IndexError):
-            self._grid.get(10, 10)
+        self.assertEqual(pineapple.get(100, 100), None)
 
 
