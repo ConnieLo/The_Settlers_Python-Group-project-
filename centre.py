05228c89257358c3ec170a9fc393a2f1cd7de083
mf479 <maruf.f@hotmail.com>
2023-03-02 12:18:06 +0000

    .

--- centre.py
+++ centre.py
@@ -1,7 +1,11 @@
 import vertex
 
-class Centre: #could be renamed to tile?
+class Tile: #could be renamed to tile?
     
     def __init__(self, resource, number):
         self.resource = resource
         self.number = number
+
+    def __str__(self):
+        return f"Tile {self.number}: {self.resource_type}"
+
--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class player:
         self.color = color
         self.score = 0
         self.resources = {"wood": 0, "brick": 0, "sheep": 0, "wheat": 0, "ore": 0}
+        self.development_cards = {"Knights": 0, "Road Building": 0, "Year of Plenty": 0, "Monopoly": 0, "University": 0, "Market": 0, "Great Hall": 0, "Chapel": 0, "Library": 0,}
 
     def increase_score(self, points):
         self.score += points
@@ -29,8 +30,21 @@ class player:
     def display(self, surface, font, x, y):
         text = font.render(f"{self.name}'s Victory Points: {self.score}", True, self.color)
         text2 = font.render(f"{self.name}'s Resources: {self.resources}", True, self.color)
+        text3 = font.render(f"{self.name}'s Development Cards: {self.development_cards}", True, self.color)
         surface.blit(text, (x, y))
         surface.blit(text2, (x, y+50))
+        surface.blit(text3, (x, y+80))
+
+    def add_development_cards(self, development_cards, quantity):
+        self.development_cards[development_cards] += quantity
+
+    def remove_development_cards(self, development_cards, quantity):
+        if self.resources[development_cards] >= quantity:
+            self.resources[development_cards] -= quantity
+            return True
+        else:
+            return False
 
     def new_settlement(self, vertex):
         vertex.add_settlement(self)
+
