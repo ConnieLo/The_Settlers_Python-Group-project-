88876f6ae6d7b2b751e9110933e7cb184ecac822
haaa20 <haaa20@sussex.ac.uk>
2023-03-03 12:57:35 +0000

    GridStruct rough implimentation of storing on vertices

--- grid_structure.py
+++ grid_structure.py
@@ -28,11 +28,21 @@ class GridStruct(GridBasic):
     def get(self, *c, f=None):
         # This will fetch a _TileWrapper object
         tile = super().get(c[0], c[1])
-
+        # If tile is none, there's nothing there
+        # Note this is NOT the same as having a tile with no data, as it may still point to edges and vertices
+        # which DO have data
         if tile is None:
             return None
 
-        return tile.val
+        # Get the tile's held value
+        if f is None:
+            return tile.val
+        # Get a vertex
+        elif f == "v":
+            pass
+        # Get an edge
+        elif f == "e":
+            pass
 
     # This is really gross I don't like it...
     # because of how Python handles unpacking args with * I just have to TRUST that c[2] is val
@@ -42,7 +52,40 @@ class GridStruct(GridBasic):
         new_tile = _TileWrapper(c[2])
         super().set(c[0], c[1], new_tile)
 
+    def _get_vertex(self, x, y, z):
+        # VERTICES
+        # z = 2 -> v 0 of neighbor 2
+        # z = 3 -> v 1 of neighbor 3
+        # z = 4 -> v 0 of neighbor 3
+        # z = 5 -> v 1 of neighbor 4
+        if z == 0 or z == 1:
+            tile = super().get(x, y)
+            return tile.v[z]
+        elif z == 2:
+            return self._get_vertex(*neighbor_of(x, y, 2), 0)
+        elif z == 3:
+            return self._get_vertex(*neighbor_of(x, y, 3), 1)
+        elif z == 4:
+            return self._get_vertex(*neighbor_of(x, y, 3), 0)
+        elif z == 5:
+            return self._get_vertex(*neighbor_of(x, y, 4), 1)
+
+    def _set_vertex(self, x, y, z, val):
+        if z == 0 or z == 1:
+            tile = super().get(x, y)
+            tile.v[z] = val
+        elif z == 2:
+            return self._set_vertex(*neighbor_of(x, y, 2), 0, val)
+        elif z == 3:
+            return self._set_vertex(*neighbor_of(x, y, 3), 1, val)
+        elif z == 4:
+            return self._set_vertex(*neighbor_of(x, y, 3), 0, val)
+        elif z == 5:
+            return self._set_vertex(*neighbor_of(x, y, 4), 1, val)
+
 
 class _TileWrapper:
-    def __init__(self, val):
+    def __init__(self, val=None):
         self.val = val
+        self.v = [None] * 2
+        self.e = [None] * 3
