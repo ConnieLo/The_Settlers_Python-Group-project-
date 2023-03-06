6821ea9f393b45afb7c60f3e590d502eb6ceec5f
fin20 <fingalletly03@gmail.com>
2023-03-06 17:18:43 +0000

    added more test cases

--- unit tests/test_grid_structure.py
+++ unit tests/test_grid_structure.py
@@ -15,4 +15,30 @@ class TestGridBasic(unittest.TestCase):
         # Test raising error if index out of bounds
         self.assertEqual(pineapple.get(100, 100), None)
 
+    def test_get_set_vertex(self):
+        banana = GridStruct()
+        banana._set_vertex(0, 0, 0, 'v')
+        banana._set_vertex(0, 1, 3, 'v')
 
+        # test value inside bounds
+        self.assertEqual(banana._get_vertex(0, 0, 0), banana._get_vertex(0, 0, 0))
+
+        # test wrong value
+        self.assertEqual(banana._get_vertex(0, 0, 0), 'e')
+
+        # test value outside bounds
+        self.assertEqual(banana._get_vertex(0, 0, 0), banana._get_vertex(1, 1, 1))
+
+    def test_get_set_edge(self):
+        mango = GridStruct()
+        mango._set_edge(0, 0, 2, 'e')
+        mango._set_edge(1, 0, 5, 'e')
+
+        # test value inside bounds
+        self.assertEqual(mango._get_edge(0, 0, 2), mango._get_edge(1, 0, 5))
+
+        # test wrong value
+        self.assertEqual(mango._get_edge(0, 0, 2), 'a')
+
+        # test value outside bounds
+        self.assertEqual(mango._get_edge(0, 0, 2), mango._get_edge(1, 1, 1))
