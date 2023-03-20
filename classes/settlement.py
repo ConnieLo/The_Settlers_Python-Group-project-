b181036e90f0fec24ff0f6a67b6f9429a07d74a4
Conrad Lovegrove <conrad.lovegrove@gmail.com>
2023-03-20 16:33:06 +0000

    updated settlement class to match UML

--- classes/settlement.py
+++ classes/settlement.py
@@ -1,10 +1,16 @@
+import player
+
 class Settlement():
 
     def __init__(self, owner, potential_resources):
         self.owner = owner  #player instance
-        self.potential_resources = potential_resources #dictionary of resource and activation number
+        self.is_city = False #dictionary of resource and activation number
+        self.potential_resources = potential_resources
 
     def grant_resources(self, resource):
-        for res in self.potential_resources:
-            if res == resource:
-                self.owner.add_resources
+        if self.is_city:
+            for res in self.potential_resources:
+                self.owner.add_resource(res, 2)
+        else:
+            for res in self.potential_resources:
+                self.owner.add_resource(res, 1)
