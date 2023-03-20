42687903eddcbea6c43b085dd30e67631432666e
Conrad Lovegrove <conrad.lovegrove@gmail.com>
2023-03-20 16:34:52 +0000

    added comments to settlement

--- classes/settlement.py
+++ classes/settlement.py
@@ -4,10 +4,11 @@ class Settlement():
 
     def __init__(self, owner, potential_resources):
         self.owner = owner  #player instance
-        self.is_city = False #dictionary of resource and activation number
-        self.potential_resources = potential_resources
+        self.is_city = False #boolean
+        self.potential_resources = potential_resources #dictionary of resource and activation number
 
     def grant_resources(self, resource):
+        #generates resources for the owner of the settlement
         if self.is_city:
             for res in self.potential_resources:
                 self.owner.add_resource(res, 2)
