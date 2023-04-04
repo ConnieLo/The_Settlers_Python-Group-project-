3d6c719ce064e2fde49aec439e43faf6feffa7f7
Hugo <haaa20@sussex.ac.uk>
2023-04-04 14:56:16 +0100

    game_master and turn - mostly shell code
    also moved the grid

--- classes/game_master.py
+++ classes/game_master.py
@@ -1,10 +1,14 @@
-import classes.grid as grid
-import classes.turn as turn
+from classes import *
 
 
 class GameMaster:
-    turn_queue: list[turn.Turn]
-    current_turn: turn.Turn
+    turn_queue: list[Turn]
+    current_turn: Turn
 
     def __init__(self):
         self.turn_queue = []
+
+    # AT the start of each turn, goes through every settlement and assigns the relevant resorces to the player
+    def pass_resources(self, roll):
+        for s in board.get_settlments():
+            s.grant_resources(roll)
--- classes/turn.py
+++ classes/turn.py
@@ -7,11 +7,23 @@ class Turn:
     active_player: player.Player
     master: gm.GameMaster
 
-    def __init__(self, master, which_player, turn_no):
+    def __init__(self, master: gm.GameMaster, which_player, turn_no):
         self.master = master
         self.turn_no = turn_no
         self.active_player = which_player
         self.roll = [randint(1, 6), randint(1, 6)]
 
-    def prod_resources(self):
-        roll_total = sum(self.roll)
+    def take_turn(self):
+        # Hand out resources among players
+        self.master.pass_resources(sum(self.roll))
+
+        # Prompt the player to do any trades, we shouldn't need to save the return value of this function
+        self.active_player.prompt_trade()
+
+        # Prompt the player to purchase/build new items. Pass any new roads or settlments to the game master
+        new_builds = self.active_player.prompt_builds()
+        self.master.update_board(new_builds)
+
+
+
+
