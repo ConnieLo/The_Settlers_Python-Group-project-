001b51e7f9a696b51a121d26ddf45566eba80882
Hugo <haaa20@sussex.ac.uk>
2023-04-04 14:44:29 +0100

    Some tweaks to settlement

--- classes/settlement.py
+++ classes/settlement.py
@@ -1,16 +1,17 @@
 from classes import player
+
 class Settlement():
 
     def __init__(self, owner, potential_resources):
-        self.owner = owner  #player instance
+        self.owner: player = owner  #player instance
         self.is_city = False #boolean
-        self.potential_resources = potential_resources #dictionary of resource and activation number
+        # Array of tuples (i, r) where i is a number and r is a string representation of a resource
+        self.potential_resources = potential_resources
 
-    def grant_resources(self, resource):
-        #generates resources for the owner of the settlement
-        if self.is_city:
-            for res in self.potential_resources:
-                self.owner.add_resource(res, 2)
-        else:
-            for res in self.potential_resources:
-                self.owner.add_resource(res, 1)
+
+    # Takes an integer and check to see if it has any resources of that number it can give to the player
+    def grant_resources(self, i):
+        for num, res in self.potential_resources:
+            if num == i:
+                self.owner.add_resource(res)
+
