d012da717ae393895f3484162fa21f27fa07253a
haaa20 <haaa20@sussex.ac.uk>
2023-04-21 15:31:03 +0100

    Docstring for GameMaster

--- classes/game_master.py
+++ classes/game_master.py
@@ -21,8 +21,39 @@ PLAYER_COLOURS = [
 ]
 
 class GameMaster:
+    """
+    The primary class responsible for maintaining a single game of The Settlers. Acts as the central management node
+    through which each other object in the game communicates. Is also responsible for instancing many of the objects
+
+    ATTRIBUTES
+
+    turn_queue: list
+        The players in the game, in turn order
+
+    current_turn: int
+        Current turn number, starting at -1 for the set-up, and incrementing by one for each player turn
+
+    turn_inst: Turn
+        The Turn object currently active
+
+    board: Board
+        The board - which holds tiles, settlements, and roads
+
+    initialised: bool
+        Whether the game has advanced beyone the set-up phase
+
+    saved_tuples: list
+        A list of all the tuples the game master has checked so far.
+        Tuples are in the format (x: num, y: num) and represent a position on the gui
+
+    tuple_count: dict
+        The amount of times the game master has seen a given tuple, in the format {tuple: int}
+
+    last_checked_tuple: tuple
+        The last tuple the game master has seen
+    """
     def __init__(self):
-        self.turn_queue: List[Turn] = [
+        self.turn_queue = [
             Player("player {}".format(i), PLAYER_COLOURS[i]) for i in range(4)
         ]
         self.current_turn: int = -1 #initialised as -1 so that first turn instance will advance it to player 0
@@ -32,15 +63,29 @@ class GameMaster:
         self.saved_tuples = []
         self.tuple_count = {}
         self.last_checked_tuple = None
-        self.initialised = False    #does nothing yet, going to use this for the start of the game when each player must place 2 settlements and roads
+        self.initialised = False
 
     def next_turn(self) -> Turn:
+        """
+        Generates a new Turn object for whichever player is next
+        :return:
+            The Turn
+        """
         self.current_turn += 1
         player = self.turn_queue[self.current_turn % 4]
         print("next turn")
         self.turn_inst = Turn(self, player, self.current_turn)
 
     def check_three_same_tuples(self, positions: list) -> bool:
+        """
+        Check to see if a list of tuples - representing a point on the board, are the same
+
+        :param positions: list
+            The positions
+
+        :return:
+            True if they are the same
+        """
         for position in positions:
             if position != self.last_checked_tuple:
                 self.saved_tuples.append(position)
@@ -53,13 +98,31 @@ class GameMaster:
                     return True
         return False
 
-    def new_settlement(self, owner: Player, settlement_type: str) -> Settlement:
+    def new_settlement(self, owner: Player, settlement_info: str) -> Settlement:
+        """
+        Signals the board to create a new settlement, and updates the player's victory points and number of
+        settlements
+
+        :param owner: Player
+            The player building the new settlement
+        :param settlement_info:
+            A data representation of the new settlement
+        :return:
+            The newly constructed settlement
+        """
         owner.increment_victory_points()
         owner.increment_number_of_settlements()
-        self.board.new_settlement(owner, settlement_type)
+        self.board.new_settlement(owner, settlement_info)
         return True
     
     def new_road(self, owner, position):
+        """
+        Signals the board to create a new road at the given position, and updates the owner's number of roads
+        :param owner: Player
+            The player building the new road
+        :param position:
+        :return:
+        """
         self.board.new_road(owner, position)
         owner.increment_number_of_roads()
         return True
@@ -77,12 +140,36 @@ pygame.font.init()
 font = pygame.font.Font(None, 36)
 # A helper method used in draw method
 def draw_text(screen, text, x, y, color=(255, 255, 255), font=None):
+    """
+    Draws the given text on screen
+
+    :param screen: Pygame.Surface
+        The surface on which the text is to be written
+    :param text: str
+        The text
+    :param x: int
+        The x co-ordinate
+    :param y: inr
+        The y co-ordinate
+    :param color: tuple, optional
+        The colour as an (r, g, b) tuple of ints
+    :param font: Pygame.Font
+        The font to use
+    """
     text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect()
     text_rect.topleft = (x, y)
     screen.blit(text_surface, text_rect)
 
 def draw(game_master: GameMaster, screen):
+    """
+    Prompts the given game master to write infor on the current player to the screen
+
+    :param game_master: GameMaster
+        The game master
+    :param screen: Pygame.Surface
+        The surface which is to be written to
+    """
     # Get the current player
     current_player = game_master.turn_queue[game_master.current_turn % 4]
     # Display the current player's turn
